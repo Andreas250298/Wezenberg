576bd3b7836353cfb1d61c6c80698d636aff8da0
User <User@ASUS-Florian>
2018-03-09 15:42:19 +0100

    Startpagina voorbereidingen

--- application/views/startpagina.php
+++ application/views/startpagina.php
@@ -1,1 +1,78 @@
-<p> dit is de startpagina <3 </p>
+<style>
+    .row-striped:nth-of-type(odd){
+        background-color: #efefef;
+    }
+
+    .row-striped:nth-of-type(even){
+        background-color: #ffffff;
+    }
+
+    .row-striped {
+        padding: 10px 0;
+    }
+</style>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            test1
+        </div>
+        <div class="col-lg-4">
+            <div class="row row-striped">
+                <div class="col-3 text-right">
+                    <h2><span class="badge badge-secondary">23</span></h2>
+                    <h4>OCT</h4>
+                </div>
+                <div class="col-9">
+                    <h5 class="text-uppercase">Belgisch kampioenschap</h5>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Maandag</li>
+                        <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 12:30 PM - 2:00 PM</li>
+                        <li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Antwerpen</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row row-striped">
+                <div class="col-3 text-right">
+                    <h2><span class="badge badge-secondary">27</span></h2>
+                    <h4>OCT</h4>
+                </div>
+                <div class="col-9">
+                    <h5 class="text-uppercase">4x100 Amsterdam</h5>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Vrijdag</li>
+                        <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 2:30 PM - 4:00 PM</li>
+                        <li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Amsterdam</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row row-striped">
+                <div class="col-3 text-right">
+                    <h2><span class="badge badge-secondary">23</span></h2>
+                    <h4>OCT</h4>
+                </div>
+                <div class="col-9">
+                    <h5 class="text-uppercase">Belgisch kampioenschap</h5>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Maandag</li>
+                        <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 12:30 PM - 2:00 PM</li>
+                        <li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Antwerpen</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row row-striped">
+                <div class="col-3 text-right">
+                    <h2><span class="badge badge-secondary">27</span></h2>
+                    <h4>OCT</h4>
+                </div>
+                <div class="col-9">
+                    <h5 class="text-uppercase">4x100 Amsterdam</h5>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Vrijdag</li>
+                        <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 2:30 PM - 4:00 PM</li>
+                        <li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Amsterdam</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
