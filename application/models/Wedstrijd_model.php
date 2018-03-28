25f17c3d30f35ec10af10a01df2117af1208ce86
Jordi Bols <r0631689@student.thomasmore.be>
2018-03-28 14:03:56 +0200

    Doxygen commentaar toegevoegd bij Wedstrijd model

--- application/models/Wedstrijd_model.php
+++ application/models/Wedstrijd_model.php
@@ -1,12 +1,26 @@
 <?php
 
+/**
+ * @class Wedstrijd_model
+ * @brief Model-klasse voor wedstrijden
+ *
+ * Model-klasse die alle methodes bevat om te interageren met de wedstrijd tabel
+ */
 class Wedstrijd_model extends CI_Model
 {
+    /**
+     * Constructor
+     */
     public function __construct()
     {
         parent::__construct();
     }
 
+    /**
+     * Een wedstrijd ophalen uit de database
+     * @param $id Het id van de wedstrijd die opgevraagd wordt
+     * @return De opgevraagde record
+     */
     public function get($id)
     {
         $this->db->where('id', $id);
@@ -14,6 +28,10 @@ class Wedstrijd_model extends CI_Model
         return $query->row();
     }
 
+    /**
+     * Opvragen van alle wedstrijden uit de database, oplopend gesorteerd
+     * \return De opgevraagde records
+     */
     public function toonWedstrijdenASC()
     {
         $this->db->order_by('beginDatum', 'asc');
@@ -21,6 +39,10 @@ class Wedstrijd_model extends CI_Model
         return $query->result();
     }
 
+    /**
+     * Opvragen van alle nieuws artikels uit de database, aflopend gesorteerd
+     * \return De opgevraagde records
+     */
     public function toonWedstrijdenDESC()
     {
         $this->db->order_by('beginDatum', 'desc');
@@ -28,18 +50,31 @@ class Wedstrijd_model extends CI_Model
         return $query->result();
     }
 
+    /**
+     * Een wedstrijd toevoegen aan de database
+     * @param $wedstrijd De wedstrijd die moet toegevoegd worden
+     * @return De insert functie van de wedstrijd
+     */
     public function insert($wedstrijd)
     {
         $this->db->insert('wedstrijd', $wedstrijd);
         return $this->db->insert_id();
     }
 
+    /**
+     * Een wedstrijd wijzigen in de database
+     * @param $wedstrijd De wedstrijd die moet gewijzigd worden
+     */
     public function update($wedstrijd)
     {
         $this->db->where('id', $wedstrijd->id);
         $this->db->update('wedstrijd', $wedstrijd);
     }
 
+    /**
+     * Een wedstrijd verwijderen uit de database
+     * @param $id Het id van de wedstrijd die moet verwijderd worden
+     */
     public function delete($id)
     {
         $this->db->where('id', $id);
