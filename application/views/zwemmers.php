b55f40f36f65f0b15fc9f2671465e6383bdbbf0a
MattiDC <deconinckmatt@gmail.com>
2018-04-18 15:06:41 +0200

    Wachten op delete deelname

--- application/views/zwemmers.php
+++ application/views/zwemmers.php
@@ -16,9 +16,9 @@ var id = "";
         $.ajax({type: "GET",
         url: site_url + "/gebruiker/verwijder",
         data:{id : id},
-        // success: function(){
-        //   window.location.reload();
-        // },
+        success: function(){
+          window.location.reload();
+        },
         error: function (xhr, status, error){
           aler("--ERROR IN AJAX --\n\n" + xhr.responseText);
         }
