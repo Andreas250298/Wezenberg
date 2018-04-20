5990796c59b287734e41f7b50547d00cf3f0ef59
sacharypens <sacharypens@gmail.com>
2018-04-20 15:07:13 +0200

    over ons pagina kan nu worden bewerkt(zonder foto's)

--- application/views/over_ons_aanpassen.php
+++ application/views/over_ons_aanpassen.php
@@ -14,47 +14,43 @@ $dataSubmit = array('class' => 'btn btn-primary my-2 my-sm0', 'value' => 'Opslaa
         font-weight: bold;
     }
 </style>
-<div class="form-row">
-    <?php echo form_open('trainingscentrum/registreer', '') ?>
-    <div class=" col-sm-8 overOns form-group">
+<div class="form-row col-12">
+    <?php echo form_open('trainingscentrum/registreer', 'class="col-12"') ?>
+    <div class=" col-12 overOns form-group">
         <?php
         echo form_label("Pas welkomtekst aan", 'welkom') . "\n";
         echo form_textarea('beschrijvingWelkom', $trainingscentrum->beschrijvingWelkom, 'class="form-control"');
         ?>
     </div>
-    <div class="col-sm-4 form-group">
-        <?php echo form_button('afbeelding', 'Nieuwe afbeelding', 'class="form-control"'); ?>
-        <?php echo form_button('wijzig', 'Wijzigen', 'class="form-control"'); ?>
+    <div class="col-12 form-group">
+        <?php echo form_button('afbeelding', 'Nieuwe afbeelding', 'class="btn-primary btn"'); ?>
     </div>
-    <div class="col-sm-8 overOns form-group">
+    <div class="col-12 overOns form-group">
         <?php
         echo form_label("Pas locatietekst aan", 'locatie') . "\n";
         echo form_textarea('beschrijvingLocatie', $trainingscentrum->beschrijvingLocatie, 'class="form-control"');
         ?>
     </div>
-    <div class="col-sm-4 form-group">
-        <?php echo form_button('afbeelding', 'Nieuwe afbeelding', 'class="form-control"'); ?>
-        <?php echo form_button('wijzig', 'Wijzigen', 'class="form-control"'); ?>
+    <div class="col-12 form-group">
+        <?php echo form_button('afbeelding', 'Nieuwe afbeelding', 'class="btn-primary btn"'); ?>
     </div>
-    <div class="col-sm-8 overOns form-group">
+    <div class="col-12 overOns form-group">
         <?php
         echo form_label("Pas teamtekst aan", 'team') . "\n";
         echo form_textarea('beschrijvingTeam', $trainingscentrum->beschrijvingTeam, 'class="form-control"');
         ?>
     </div>
-    <div class="col-sm-4 form-group">
-        <?php echo form_button('afbeelding', 'Nieuwe afbeelding', 'class="form-control"'); ?>
-        <?php echo form_button('wijzig', 'Wijzigen', 'class="form-control"'); ?>
+    <div class="col-12 form-group">
+        <?php echo form_button('afbeelding', 'Nieuwe afbeelding', 'class="btn-primary btn"'); ?>
     </div>
-    <div class="col-sm-8 overOns form-group">
+    <div class="col-12 overOns form-group">
         <?php
         echo form_label("Pas trainertekst aan", 'welkom') . "\n";
         echo form_textarea('beschrijvingTrainer', $trainingscentrum->beschrijvingTrainer, 'class="form-control"');
         ?>
     </div>
-    <div class="col-sm-4 form-group">
-        <?php echo form_button('afbeelding', 'Nieuwe afbeelding', 'class="form-control"'); ?>
-        <?php echo form_button('wijzig', 'Wijzigen', 'class="form-control"'); ?>
+    <div class="col-12 form-group">
+        <?php echo form_button('afbeelding', 'Nieuwe afbeelding', 'class="btn-primary btn"'); ?>
     </div>
 
     <?php echo form_submit($dataSubmit) . "";
