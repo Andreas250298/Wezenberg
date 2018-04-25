885917bb5b99fbc14b88cf8b9d5e3e5140f04fd2
User <User@ASUS-Florian>
2018-04-25 14:33:57 +0200

    Tutorial aangemaakt

---
+++ application/views/Nieuws/tutorial.php
@@ -0,0 +1,4 @@
+<h2 class="paginaTitel"><i class="fas fa-angle-left"></i> Tutorial : Nieuws beheren</h2>
+<div class="alert alert-dark" role="alert">
+  <i class="far fa-question-circle fa-2x"></i><span class="tab">Hoe nieuws beheren? <?php echo anchor('nieuws/tutorial', "Bekijk de tutorial", "")?>
+</div>
