0cef33a28bebfc9aa274f2996c98b05f49aee132
sacharypens <sacharypens@gmail.com>
2018-05-04 15:28:45 +0200

    aanpassen van afbeeldingen in trainingscentrum

--- application/controllers/Trainingscentrum.php
+++ application/controllers/Trainingscentrum.php
@@ -73,6 +73,41 @@ class Trainingscentrum extends CI_Controller {
         $trainingscentrum->beschrijvingTeam = $this->input->post('beschrijvingTeam');
         $trainingscentrum->beschrijvingTrainer = $this->input->post('beschrijvingTrainer');
 
+        $config['upload_path']          = './uploads/info';
+        $config['allowed_types']        = 'gif|jpg|jpeg|png';
+
+
+        $this->load->library('upload', $config);
+        $this->load->model('trainingscentrum_model');
+        if($this->upload->do_upload('locatie')){
+          $upload_data = $this->upload->data();
+          $oudTrainingscentrum = $this->trainingscentrum_model->get(1);
+          $this->load->helper("file");
+          unlink('uploads/info/' . $oudTrainingscentrum->fotoLocatie);
+          $trainingscentrum->fotoLocatie = $upload_data['file_name'];
+        }
+        if($this->upload->do_upload('team')){
+          $upload_data = $this->upload->data();
+          $oudTrainingscentrum = $this->trainingscentrum_model->get(1);
+          $this->load->helper("file");
+          unlink('uploads/info/' . $oudTrainingscentrum->fotoTeam);
+          $trainingscentrum->fotoTeam = $upload_data['file_name'];
+        }
+        if($this->upload->do_upload('trainer')){
+          $upload_data = $this->upload->data();
+          $oudTrainingscentrum = $this->trainingscentrum_model->get(1);
+          $this->load->helper("file");
+          unlink('uploads/info/' . $oudTrainingscentrum->fotoTrainer);
+          $trainingscentrum->fotoTrainer = $upload_data['file_name'];
+        }
+        if($this->upload->do_upload('welkom')){
+          $upload_data = $this->upload->data();
+          $oudTrainingscentrum = $this->trainingscentrum_model->get(1);
+          $this->load->helper("file");
+          unlink('uploads/info/' . $oudTrainingscentrum->fotoWelkom);
+          $trainingscentrum->fotoWelkom = $upload_data['file_name'];
+        }
+
         $this->load->model('trainingscentrum_model');
         $this->trainingscentrum_model->update($trainingscentrum);
 
