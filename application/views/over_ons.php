261e83a04a95c27e303f1931da00a0e3912ea8f4
sacharypens <sacharypens@gmail.com>
2018-05-04 14:46:04 +0200

    over ons linken afbeelding opgelost

--- application/views/over_ons.php
+++ application/views/over_ons.php
@@ -13,10 +13,10 @@
         <?php echo $trainingscentrum->beschrijvingWelkom; ?>
     </div>
     <div class="col-lg-12 overOns">
-        <?php echo '<img src="' . base_url($trainingscentrum->fotoWelkom) . '" width="100%"/>'; ?>
+        <?php echo '<img src="' . base_url('uploads/info/' . $trainingscentrum->fotoWelkom) . '" width="100%"/>'; ?>
     </div>
     <div class="col-lg-4 d-none d-lg-block overOns">
-        <?php echo '<img src="' . base_url($trainingscentrum->fotoLocatie) . '" width="100%"/>'; ?>
+        <?php echo '<img src="' . base_url('uploads/info/' .$trainingscentrum->fotoLocatie) . '" width="100%"/>'; ?>
     </div>
     <div class="col-lg-8 overOns">
         <h2>Locatie Wezenberg</h2>
@@ -27,10 +27,10 @@
         <?php echo $trainingscentrum->beschrijvingTeam; ?>
     </div>
     <div class="col-lg-4 overOns">
-        <?php echo '<img src="' . base_url($trainingscentrum->fotoTeam) . '" width="100%"/>'; ?>
+        <?php echo '<img src="' . base_url('uploads/info/' .$trainingscentrum->fotoTeam) . '" width="100%"/>'; ?>
     </div>
     <div class="col-lg-4 d-none d-lg-block overOns">
-        <?php echo '<img src="' . base_url($trainingscentrum->fotoTrainer) . '" width="100%"/>'; ?>
+        <?php echo '<img src="' . base_url('uploads/info/' .$trainingscentrum->fotoTrainer) . '" width="100%"/>'; ?>
     </div>
     <div class="col-lg-8 overOns">
         <h2>De trainers</h2>
