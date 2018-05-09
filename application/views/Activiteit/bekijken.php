ddc90c7ad38f783d1e632859b84f65077c5cfbc2
jordibols <r0631689@student.thomasmore.be>
2018-05-09 10:44:37 +0200

    ID niet langer tonen bij activiteit

--- application/views/Activiteit/bekijken.php
+++ application/views/Activiteit/bekijken.php
@@ -137,7 +137,7 @@ $uren = array('1' => "07:00", '2' => "08:00", '3' => "09:00", '4' => "10:00", '5
                   foreach ($activiteiten as $activiteit)
                   {
                           echo "<div id='" . $activiteit->andereActiviteit->id . "' class='gebeurtenis " . $activiteit->andereActiviteit->beginDatum . " " . $activiteit->andereActiviteit->tijdstip . " ander" . $teller . "'>";
-                          echo "<span><b>" . $activiteit->andereActiviteit->naam . " ID: " . $activiteit->andereActiviteit->id . "</b></span><br />";
+                          echo "<span><b>" . $activiteit->andereActiviteit->naam . "</b></span><br />";
                           if ($activiteit->andereActiviteit->soortId == 1)
                           {
                             echo "Vertrek: " . zetOmNaarDDMMYYYY($activiteit->andereActiviteit->beginDatum) . "<br />";
